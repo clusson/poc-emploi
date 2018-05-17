5f6048daf5ba94b8193d9b9b82915016f80661d5
Sylvain <sylvain.denyse@gmail.com>
2018-05-17 10:35:14 +0200

    fit mouseEvent data

--- src/main.js
+++ src/main.js
@@ -7,41 +7,29 @@ import router from './router';
 import Buefy from 'buefy';
 
 import 'buefy/lib/buefy.css';
-import 'vuetify/dist/vuetify.min.css';
+import 'vuetify/dist/vuetify.min.css'
 
-import heatmap from 'vue-heatmapjs';
-import io from 'socket.io-client';
+import heatmap from 'vue-heatmapjs'
+import io from 'socket.io-client'
 
-Vue.use(Vuetify);
-Vue.use(Buefy);
+Vue.use(Vuetify)
+Vue.use(Buefy)
 
-const socket = io.connect(process.env.SOCKETIO_DEVOUR_URI);
+const socket = io.connect(process.env.SOCKETIO_DEVOUR_URI)
 
 Vue.use(heatmap, {
   afterAdd(data) {
     const mouseEvent = {
-      clientX: data.clientX,
-      clientT: data.clientY,
-      layerX: data.layerX,
-      layerY: data.layerY,
-      movementX: data.movementX,
-      movmementY: data.movementY,
-      offsetX: data.offsetX,
-      offsetY: data.offsetY,
-      pageX: data.pageX,
-      pageY: data.pageY,
-      screenX: data.screenX,
-      screenY: data.screenY,
-      URI: data.target.baseURI,
-      type: data.type,
-      value: data.value,
       x: data.x,
-      y: data.y
-    };
-    socket.emit('feed', mouseEvent);
+      y: data.y,
+      value: data.value,
+      type: data.type,
+      URI: data.target.baseURI
+    }
+    socket.emit('feed', mouseEvent)
   }
-});
-Vue.config.productionTip = false;
+})
+Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
@@ -49,4 +37,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-});
+})
